7a2461ab5b7b70224f9c75d6e912ccd016ae72c4
Haseeb Majid <hello@haseebmajid.dev>
2024-01-02 00:01:38 +0000

    feat: move rpi hosts to this repo

--- flake.nix
+++ flake.nix
@@ -3,18 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hardware.url = "github:nixos/nixos-hardware";
+    sops-nix.url = "github:mic92/sops-nix";
+    colmena.url = "github:zhaofengli/colmena";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
+  outputs = {
+    self,
+    nixpkgs,
+    colmena,
+    ...
+  } @ inputs: {
+    colmena = import ./hosts/colmena.nix {inherit nixpkgs inputs;};
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
       pkgs.mkShell {
-        packages = with pkgs;[
-					go
-					pulumi 
-					pulumiPackages.pulumi-language-go
+        packages = with pkgs; [
+          go
+          pulumi
+          pulumiPackages.pulumi-language-go
         ];
       };
   };
